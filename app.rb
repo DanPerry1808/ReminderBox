d65b12fef5d5e2f64b562bfeb2819fad992d1efc
Dan Perry <danperry1808@gmail.com>
2021-08-05 11:15:50 +0100

    Added IFTTT integration, now sends notifications to phone

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require "sinatra"
 require "require_all"
 require "date"
+require "net/https"
+require "uri"
+require "socket"
+require "json"
 
 require_rel "db", "models"
 
@@ -29,7 +33,7 @@ end
 
 # View for displaying all current reminders
 get '/view_all' do
-    @rems = Reminder.all.order(:date)
+    @rems = Reminder.order(:date)
     erb :all
 end
 
@@ -45,4 +49,46 @@ get '/date/:date' do
     @pm_rems = Reminder.where(date: @date_obj, time: "PM")
 
     erb :list
+end
+
+# Cron jobs will send a request to this path at set times to send notifications
+# to my phone through IFTTT
+# Only acceptable values for :time param are 'AM' and 'PM'
+get '/cron/:time' do
+
+    # Check param was sent
+    if params[:time].nil? || params[:time].empty?
+        status 400
+        return "Bad request. No time specified"
+    end
+
+    time_text = nil
+    if params[:time] == "AM"
+        time_text = "morning"
+    elsif params[:time] == "PM"
+        time_text = "evening"
+    else
+        status 400
+        return "Bad request. Invalid time specified"
+    end
+
+    # Get the number of reminders for the current day and time
+    num_rems = Reminder.where(date: Date.today, time: params[:time]).count.to_s
+
+    # Get own IP address to send as the URL for the IFTTT notification
+    ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+    this_ip = ip.ip_address
+    notif_address = "http://#{this_ip}:4567/now"
+
+    # Get the URL for the IFTTT notification
+    uri = URI.parse("https://maker.ifttt.com/trigger/RemBoxNotif/with/key/#{ENV["IFTTTKEY"]}")
+    # Must use HTTPS to make requests to IFTTT
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.body = {value1: num_rems, value2: time_text, value3: notif_address}.to_json
+    request["Content-Type"] = "application/json"
+    # Make the request to IFTTT
+    response = https.request(request)
+    return "done"
 end
